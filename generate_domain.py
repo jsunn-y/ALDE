89b7d4d4288a8c7303ce73188ef5caf7b3231747
jsunn-y <jasonyang12345@gmail.com>
2024-04-02 15:17:36 -0700

    updated argparser

--- generate_domain.py
+++ generate_domain.py
@@ -1,25 +1,32 @@
 import numpy as np
 import pandas as pd
+import argparse
 import torch
 from src.encoding_utils import generate_onehot, generate_all_combos
 
 """
 Script to generate a domain for the combinatorial library, for a production (wet-lab) run. Only needs to be run once before an ALDE campaign.
 """
-#set the number of residues in the combinatorial library and the name for the campaign
-nsites = 5
-name = 'ParPgb'
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--name", type=str, default="ParPgb")
+    parser.add_argument("--nsites", type=int, default=5)
+    args = parser.parse_args()
+    
+    #set the number of residues in the combinatorial library and the name for the campaign
+    nsites = args.nsites
+    name = args.name
 
-#set path for to store results
-path = 'data/' + name + '/'
+    #set path for to store results
+    path = 'data/' + name + '/'
 
-#generate strings for all combos in the design space
-all_combos = generate_all_combos(nsites)
-df = pd.DataFrame(all_combos, columns=['Combo'])
-df.to_csv(path + 'all_combos.csv', index=False)
+    #generate strings for all combos in the design space
+    all_combos = generate_all_combos(nsites)
+    df = pd.DataFrame(all_combos, columns=['Combo'])
+    df.to_csv(path + 'all_combos.csv', index=False)
 
-#generate onehot encoding for all combos
-X = torch.reshape(generate_onehot(all_combos), (len(all_combos), -1))
-torch.save(X, path + 'onehot_x.pt')
+    #generate onehot encoding for all combos
+    X = torch.reshape(generate_onehot(all_combos), (len(all_combos), -1))
+    torch.save(X, path + 'onehot_x.pt')
 
-#implement your own code to generate other desired encodings
+    #implement your own code to generate other desired encodings
